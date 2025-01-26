d2d1dc5ef96c70a6945e20c34e63c0cd23dc20e9
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2025-01-26 03:13:56 -0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Ignore unused variables and imports
+      "import/no-anonymous-default-export": "off", // Ignore anonymous default export warnings
+    },
+    overrides: [
+      {
+        files: ["./src/app/components/header.tsx"], // Apply rule changes only to the header component
+        rules: {
+          "@typescript-eslint/no-unused-vars": "off",
+        },
+      },
+      {
+        files: ["./src/sanity/schemaTypes/product.ts"], // Apply rule changes only to the product schema
+        rules: {
+          "import/no-anonymous-default-export": "off",
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
