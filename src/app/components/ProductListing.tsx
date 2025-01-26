c4d162d1ee9ab35267c588f3fadcd2b9fc6d4113
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2025-01-26 03:30:15 -0800

    Update ProductListing.tsx

--- src/app/components/ProductListing.tsx
+++ src/app/components/ProductListing.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Image from 'next/image';
 import { urlFor } from '@/sanity/lib/image';
 import Link from 'next/link';
-import product from '@/sanity/schemaTypes/product'
 
 const ProductListing = ({ product }: { product: Product }) => {
     return (
